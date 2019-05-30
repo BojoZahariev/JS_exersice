ba86399885655dcd5e7311672828477da4bbf8c3
Bojo <bozhidar_zahariev@hotmail.com>
2019-05-30 06:27:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var text = document.getElementById("text");
 text.textContent
 
 
-
+//Prototype Attribute of Objects Created With a Constructor Function
 function book(title, author, pages, read) {
     this.title = title;
     this.autor = author;
@@ -15,3 +15,14 @@ function book(title, author, pages, read) {
 
 const book1 = new book("Vinetou", "Karl May", "450", "read it twice");
 text.textContent = (book1.info());
+
+//Prototype Attribute of Objects Created with new Object () or Object Literal
+var userAccount = new Object();
+var userAccount = {
+    name: "Mike"
+}
+console.log(userAccount.name);
+
+//The constructor in this example is Object () 
+var myObj = new Object();
+console.log(myObj.constructor); // Object()
